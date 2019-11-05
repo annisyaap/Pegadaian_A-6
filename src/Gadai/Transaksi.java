9169200d788a594c9972fc95b893ba2b9ed27c3b
putrayusifin <52587456+putrayusifin@users.noreply.github.com>
2019-11-05 20:29:04 +0700

    transaksi version 2

--- src/Gadai/Transaksi.java
+++ src/Gadai/Transaksi.java
@@ -18,6 +18,23 @@ public class Transaksi {
 	
 	//method  mengurangi hutang set this hutang = hutang - jumlah bayar
 	public void bayarHutang (int jumlah) {
+		this.hutang = this.hutang - jumlah;
+		
+		// ubah status
+		if (this.hutang == 0) {
+			this.status = "lunas";
+		}
+	}
 	
+	public int cariIndexTransaksi (ArrayList<Transaksi> transaksi, int id) {
+		int indexArrayListTransaksi = 0;
+		int resultIndex=0;
+		while (transaksi.get(indexArrayListTransaksi) != null) {
+			if(transaksi.get(indexArrayListTransaksi).agunan.id == id) {
+				resultIndex = indexArrayListTransaksi;
+			}
+			indexArrayListTransaksi++;
+		}
+		return resultIndex;
 	}
 }
