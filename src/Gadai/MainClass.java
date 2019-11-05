c3cefbdf4dfec585a3b6a5b7090f144208eafdce
putrayusifin <52587456+putrayusifin@users.noreply.github.com>
2019-11-05 20:40:04 +0700

    mainclass ver 2
    rum method

--- src/Gadai/MainClass.java
+++ src/Gadai/MainClass.java
@@ -1,4 +1,4 @@
-package Gadai;
+package tugas1;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -16,13 +16,12 @@ public class MainClass {
 
 		
 		ArrayList <Transaksi>transaksi = new ArrayList <Transaksi>();
-		transaksi.add(new Transaksi(agunan.get(0), nasabah.get(0), "gadai", 5000000));
+		transaksi.add(new Transaksi(agunan.get(0), nasabah.get(0), "gadai", agunan.get(0).harga));
 		transaksi.add(new Transaksi(agunan.get(1), nasabah.get(0), "gadai", 5000000));
 
 		//
 		
 		Scanner input = new Scanner (System.in);
-		
 		//MENU 2 STARTS FROM HERE
 		// Display table header transaksi
 		System.out.println ("====================================================");
@@ -31,16 +30,35 @@ public class MainClass {
 		System.out.println ("====================================================");
 		
 		// Display table data transaksi
-		ArrayList data = new ArrayList();
-		int indexData = 0;
+		
+		
 		for (Transaksi n : transaksi) {
 			if (n.status == "gadai") {
-				data.add(transaksi.get(indexData));
+				
 				String print = String.format("%5s %12s %12s %12s", n.agunan.id, n.agunan.kategoriProduk, n.status, n.hutang);
 				System.out.println (print);
 			}
 		}
 		
+		// Masukkan ID yang ingin ditebus
+		System.out.print ("Masukkan ID yang ingin ditebus: ");
+		int id = input.nextInt();
+		
+		// Find Index Object in ArrayList
+		int indexArrayListTransaksi = 0;
+		for(Transaksi n : transaksi) {
+			if(n.agunan.id == id) {
+				break;
+			}
+			indexArrayListTransaksi++;
+		}
+		
+		//Masukkan jumlah yang ingin ditebus
+		System.out.print ("Masukkan Jumlah yang ingin ditebus: ");
+		int jumlah = input.nextInt();
+		
+		//METHOD MENGURANGI HUTANG DENGAN JUMLAH YANG DIBAYAR
+		transaksi.get(indexArrayListTransaksi).bayarHutang(jumlah);
+		
 	}
-
 }
