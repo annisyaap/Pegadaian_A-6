13fc01164b56b494ec65bb90977b823455b3270b
basdos <basdos7@gmail.com>
2019-11-05 20:08:08 +0700

    Update MainClass versi 2

--- src/Gadai/MainClass.java
+++ src/Gadai/MainClass.java
@@ -11,17 +11,19 @@ public class MainClass {
 		ArrayList<Nasabah> n = new ArrayList<>();
 		ArrayList<Transaksi> t = new ArrayList<>();
 		
-		String status="", kategoriProduk = "", deskripsiProduk = "";
+		String status="gadai", kategoriProduk = "laptop", deskripsiProduk = "masih baru nih";
+		n.add(new Nasabah("Bastian Dolly"));
 		int hutang=0,id=0,harga = 0;
 		
 		a.add(new Agunan(id,kategoriProduk, deskripsiProduk, harga));
 		t.add(new Transaksi(a.get(0),n.get(0),status,hutang));
+		System.out.printf(  "%5s %18s %10s %16s %10s", "ID", "Nama", "Product", "Description", "Price");
+		System.out.println();
+			for (Transaksi t1:t) {
+				if(t1.status== "gadai") {
+					System.out.printf(  "%5s %18s %10s %16s %10s",t1.agunan.id,t1.nasabah.namaNasabah,t1.agunan.kategoriProduk,t1.agunan.deskripsiProduk ,t1.hutang);
+			}
 		
-		System.out.println("----------------------------------------------------------------------");
-		System.out.println("| ID |       Nama       |  Product  |       Description    |  Price  |");
-		System.out.println("----------------------------------------------------------------------");
-		for (Transaksi t1:t) {
-			System.out.println("|  " +a.get(0) + "  |         "+t.get(1)+ "\t|  "+a.get(1)+"  |       "+a.get(2) +"    |  "+t.get(3)+"  |");
 		}
 	}
 
