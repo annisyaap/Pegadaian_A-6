7b447dc61f8bcaf50ae0706c9ccc241f81003507
wasiswtj <wasiswitjaksono@gmail.com>
2019-11-05 15:44:51 +0700

    Update Agunan.java

--- src/Gadai/Agunan.java
+++ src/Gadai/Agunan.java
@@ -2,14 +2,12 @@ package Gadai;
 
 public class Agunan {
 	int id;
-	String namaProduk;
 	String kategoriProduk;
 	String deskripsiProduk;
 	int harga;
 	
-	public Agunan(int id, String namaProduk, String kategoriProduk, String deskripsiProduk, int harga) {
+	public Agunan(int id, String kategoriProduk, String deskripsiProduk, int harga) {
 		this.id = id;
-		this.namaProduk = namaProduk;
 		this.kategoriProduk = kategoriProduk;
 		this.deskripsiProduk = deskripsiProduk;
 		this.harga = harga;
@@ -18,10 +16,6 @@ public class Agunan {
 	
 	public int getId() {
 		return id;
-	}
-
-	public String getNamaProduk() {
-		return namaProduk;
 	}
 
 	public String getKategoriProduk() {
@@ -38,10 +32,6 @@ public class Agunan {
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public void setNamaProduk(String namaProduk) {
-		this.namaProduk = namaProduk;
 	}
 
 	public void setKategoriProduk(String kategoriProduk) {
